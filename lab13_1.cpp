acaa6d3fa55871c48fee1efd135e87b1f1e1c9f4
JJ <kongpopsattarach.edu@gmail.com>
2024-01-02 16:09:41 +0700

    finshlab13_1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,35 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double AM = 0,SD = 0,GM = 0,HM = 0,Max = A[0],Min = A[0],Xbar = 0,XSD = 0,XGM = 1,XHM = 0;
+    for (int i = 0; i < N; i++)
+    {
+        Xbar += A[i];
+        XGM *= A[i];
+        XHM += (double) 1/A[i];
+        if (A[i]>Max)
+        {
+            Max = A[i];
+            B[4] = Max;
+        }
+        if (A[i] < Min)
+        {
+            Min = A[i];
+            B[5] = Min;
+        }
+    }
+    AM = Xbar/N;
+    B[0] = AM;
+    for (int i = 0; i < N; i++)
+    {
+        XSD += pow(A[i]-AM,2);
+    }
+    SD = sqrt(XSD/(N));
+    B[1] = SD;
+    GM = pow(XGM,(double)1/N);
+    B[2] = GM;
+    HM = N/(XHM);
+    B[3] = HM;
+}
